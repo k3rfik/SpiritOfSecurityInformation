e9b47e325c30428bbc08a5db6b1ba20a976335a5
k3rf <luka.dorohin@yandex.ru>
2025-12-05 07:34:58 -0500

    Fix sqli.js

--- sqli.js
+++ sqli.js
@@ -2,10 +2,9 @@ var express = require('express')
 var app = express()
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize('database', 'username', 'password', {
- dialect: 'sqlite',
- storage: 'data/juiceshop.sqlite'
+	dialect: 'sqlite',
+	storage: 'data/juiceshop.sqlite'
 });
 app.post('/login', function (req, res) {
- sequelize.query('SELECT * FROM Products WHERE name LIKE ' +
-	 .body.username);
- })
+	sequelize.query('SELECT * FROM Products WHERE name LIKE ' +  .body.username);
+	})
